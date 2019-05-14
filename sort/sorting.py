517990956183c1d327014b1649f009d07be74b8a
xuesong_work <xuesong.mac@hotmail.com>
2019-05-14 11:08:34 +0800

    bubble sort

--- sort/sorting.py
+++ sort/sorting.py
@@ -12,7 +12,19 @@ def insertion_sort(array):
     return array
 
 
+def bubble_sort(array):
+    for i in range(0, len(array)):
+        for j in range(0, i):
+            if array[j + 1] < array[j]:
+                temp = array[j + 1]
+                array[j + 1] = array[j]
+                array[j] = temp
+    return array
+
+
+
 if __name__ == "__main__":
-    A = [1, 3, 6, 2, 5]
-    A = insertion_sort(A)
+    #A = [1, 3, 6, 2, 5]
+    A = [1]
+    A = bubble_sort(A)
     print(A)
